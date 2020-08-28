498ddc5af86460ffe2382e4a7820b5e3fa392dcb
MidhanshDev <68544626+MidhanshDev@users.noreply.github.com>
2020-08-28 10:35:00 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -9,10 +9,23 @@ class Box {
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.Visibility = 255;
       
       World.add(world, this.body);
     }
     display(){
+      console.log(this.body.speed);
+      if(this.body.speed < 3){
+        this.body.display;
+      }
+      else{
+        World.remove(world,this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        tint(255,this.Visibility);
+        image(this.image,this.body.position.x,this.body.position.y,50,50)
+        pop();
+      }
       var pos =this.body.position;
       var angle = this.body.angle;
       push();
--- sketch.js
+++ sketch.js
@@ -88,3 +88,8 @@ function mouseDragged(){
 function mouseReleased(){
  sling.fly();
 }
+function keyPressed(){
+  if(keyCode === 32){
+    sling.attach(polygon.body);
+  }
+}
